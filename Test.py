4a16d302f68e2432c6d210c40de7fcc6e021fdf5
KaveeN <vpdkkaveen@gmail.com>
2025-12-17 18:59:53 +0530

    added day eight content

---
+++ Test.py
@@ -0,0 +1,39 @@
+# import pdb
+
+# print("Starting the program")
+
+# x = 5
+# pdb.set_trace()
+# y = 0
+
+# z = x / y
+
+# print("End of the program")
+
+# # type n for next line
+# # type j line_number to jump to a specific line
+# # type c to continue execution
+# # type q to quit debugging session
+# # type l to list code
+# # type p variable_name to print variable value
+# # type s to step into a function
+# # type r to run until the current function returns
+# # type b line_number to set a breakpoint
+# # type clear line_number to clear a breakpoint
+# # type h for help on pdb commands
+# # type w to see the current stack trace
+# # type ! command to execute a shell command
+# # type pp variable_name to pretty-print a variable
+# # type alias to create custom commands
+
+import pdb
+
+print("Starting the program")
+
+x = 5
+breakpoint()
+y = 0
+
+z = x / y
+
+print("End of the program")
